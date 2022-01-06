20eb310388fd2fba125494313415b4ba72c59b82
user <nix_olympica@tut.by>
2022-01-06 18:49:30 +0300

    save_007 minor

--- src/core/Launch.java
+++ src/core/Launch.java
@@ -1,10 +1,12 @@
 package core;
 
+import minor.Delta;
+
 public class Launch {
 
 	public static void main(String[] args) {
 		System.out.println("..start the program");
-
+		new Delta().methodDelta_1();
 	}
 
 }
--- src/minor/Delta.java
+++ src/minor/Delta.java
@@ -1,5 +1,12 @@
 package minor;
 
 public class Delta {
-
+	int d = 1000;
+	public Delta() {
+		
+	}
+	
+	public void methodDelta_1() {
+		System.out.println("methodDelta_1");
+	}
 }
