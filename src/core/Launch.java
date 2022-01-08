55d6f98e53dba8d8e5d3598a3bf9a0480bef9777
user <nix_olympica@tut.by>
2022-01-08 20:12:41 +0300

    save_021

--- src/core/Launch.java
+++ src/core/Launch.java
@@ -7,15 +7,15 @@ public class Launch {
 	public static void main(String[] args) {
 		System.out.println("..start the program");
 
-		new Delta().methodDelta_1();
+		//new Delta().methodDelta_1();
 
-//		new Betta().methodBetta_1();
+		new Betta().methodBetta_1();
+		new Betta().methodBetta_2();
+		new Betta().methodBetta_1();
+		
 //		new Betta().methodBetta_2();
-
-		//new Betta().methodBetta_1();
-		new Betta().methodBetta_2();
-		new Betta().methodBetta_2();
-		new Betta().methodBetta_3();
+//		new Betta().methodBetta_2();
+//		new Betta().methodBetta_3();
 
 
 	}
